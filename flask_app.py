50c0f071f3b026ec2ff76a452b3c8aab5cb0f16c
lucaskauer <lucasgkauer@gmail.com>
2021-12-18 23:00:42 +0000

    add templates

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,22 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def home():
+    return render_template('index.html', title='Home')
 
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html', title='Projects')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html', title='Blog')
